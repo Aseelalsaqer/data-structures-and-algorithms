61dc9a5fbc63e668d5aa2d6120383b21d86d02cb
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-09-13 23:29:52 +0300

    merge fixes

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -1,13 +1,9 @@
 'use strict';
 
 /* ------------------------------------------------------------------------------------------------
-
 CHALLENGE 1 - Review
-
 Use the characters data below for all of the challenges except challenge 2 and 3.
-
 Write a function named sortByChildren that sorts the characters below by the number of children in each house (fewest to most). If a house has the same number of children, sort alphabetically by house name.
-
 ------------------------------------------------------------------------------------------------ */
 let characters = [
   {
@@ -62,23 +58,18 @@ const sortByChildren = (charArray) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
 Write a function named containsW that takes in a string. This function should use a regular expression pattern to return true if the string contains the letter 'w' in lower case or false if it does not. 
-
 ------------------------------------------------------------------------------------------------ */
 
 const containsW = (str) => {
-regex-1
   let word=/w/g;
 
   return word.test(str);
-
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
 Write a function named isNum that takes in a string or number of any length. This function should use a regular expression pattern to return true if the input contains a number, and false if the input does not contain a number.
-
 For example:
 12345 returns true
 '12345' returns true
@@ -87,43 +78,35 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const isNum = (input) => {
- regex-1
   let word=/[0-9]/g;
   return word.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 Write a function named containsWorld that takes in a string or number of any length. This function should use a regular expression pattern to return true if the input contains the word 'world' all in lower-case letters, and false if the input does not.
-
 ------------------------------------------------------------------------------------------------ */
 
 const containsWorld = (input) => {
- regex-1
   let word=/world/g;
   return word.test(input);
 };
+
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
-
 Write a function named isCapitalized that takes in a string. This function should use a regular expression pattern to match all words that begin with a capital letter. It should only match words, not punctuation.
-
 Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
- regex-1
   let word=/[A-Z](\w)*/g;
   if(str.match(word)){
     return str.match(word);
   }else return [];
 
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
-
 Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
 ------------------------------------------------------------------------------------------------ */
 
@@ -135,13 +118,9 @@ const citiesAtoJ = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
-
 You have created a game application and begin by asking users an easy question: In which month is Halloween?
-
 Write a function named matchMonth which uses a regular expression pattern to match any of these inputs: October, Oct, october, oct
-
 If the user enters any of these four inputs, return true. For any other input, return false.
-
 Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
@@ -152,11 +131,8 @@ const matchMonth = (input) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
-
 Write a function named noPunctuation that contains a regular expression pattern to find all of the words that contain a space immediately at the end of the word. Return an array of all such words, still containing the space at the end.
-
 For example, if given the string "Hello, and have a wonderful day!", the word "Hello, " would not be returned because it is immediately followed by a comma. The word "day!" would not be returned because it is immediately followed by an exclamation point.
-
 The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "a ", "wonderful "].
 ------------------------------------------------------------------------------------------------ */
 
@@ -167,13 +143,9 @@ const noPunctuation = str => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
-
 You want to teach a friend how to play hangman and want to show them using a partially complete puzzle.
-
 Write a function named hangman which uses the replace method to remove all of the vowels (a, e, i, o, u) from the hangman string, regardless of capitalization, and replace them with an underscore.
-
 The function should return a string containing the consonants in their original positions and underscores where the vowels were previously located.
-
 For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
@@ -184,11 +156,8 @@ let hangman = (str) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
-
 Write a function named findShells that takes in the string below and uses a regular expression pattern to find all instances of the following words: "sells", "shells", "seashells".
-
 Do not use the vertical bar (pipe) character.
-
 Hint: All of these words end with the letters "ells".
 ------------------------------------------------------------------------------------------------ */
 
@@ -201,13 +170,9 @@ const findShells = (str) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenges-04.solution.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
