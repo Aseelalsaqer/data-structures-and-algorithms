0de446e436ad54a224f3f5975c16f2439b004333
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-10-10 23:06:44 +0300

    still missing white boared

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -6,4 +6,13 @@ def reverse_list(ll):
         linked list in reversed form
     """
     # put your function implementation here
+
+
+def array_everse(ll):
+
     return ll
+
+
+ll = [1, 2, 3, 4, 5, 6]
+for i in range(len(ll)-1, -1, -1):
+    print(ll[i]),
