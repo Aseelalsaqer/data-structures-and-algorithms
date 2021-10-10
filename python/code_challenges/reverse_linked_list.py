50ba4c3b52557e8bdbc729ae9783b44ee4403238
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-10-10 23:22:12 +0300

    without a built in function

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -8,11 +8,24 @@ def reverse_list(ll):
     # put your function implementation here
 
 
+# def array_everse(ll):
+
+#     return ll
+
+
+# ll = [1, 2, 3, 4, 5, 6]
+# for i in range(len(ll)-1, -1, -1):
+#     print(ll[i]),
+
 def array_everse(ll):
+    arr = []
+    for i in ll:
+        arr.insert(0, i)
+    return arr
 
-    return ll
+
+print(array_everse([1, 2, 3, 4]))
 
 
-ll = [1, 2, 3, 4, 5, 6]
-for i in range(len(ll)-1, -1, -1):
-    print(ll[i]),
+def rev(ll):
+    return ll[::-1]
