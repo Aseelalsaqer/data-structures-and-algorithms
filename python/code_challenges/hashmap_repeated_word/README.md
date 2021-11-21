556f50103a014853353b322829f407b854aa8cdd
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-11-21 22:46:59 +0200

    missing white boared

---
+++ python/code_challenges/hashmap_repeated_word/README.md
@@ -0,0 +1,15 @@
+PR Link : https://github.com/Aseelalsaqer/data-structures-and-algorithms/pull/39
+
+# Challenge Summary
+Find the first repeated word in a book using hash table
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
+time : o(n)
+
+
+## test:
+<img src = "test31.PNG">
+python/tests/test_repeated.py
--- python/code_challenges/hashmap_repeated_word/code.py
+++ python/code_challenges/hashmap_repeated_word/code.py
@@ -1,19 +1,12 @@
-import re
-words_pattern = '[a-zA-Z]+'
+def repeated_word(value):
+    sp= value.split(" ")
+    h = {}
+    for ele in sp :
+      if ele in h:
+        return ele
+      else:
+        h[ele.lower()]=0
 
 
-def repeated_word(string: str) -> str:
-    string =string.lower()
-    array_of_words = re.findall(words_pattern, string, flags=re.IGNORECASE)
-    dic = {}
-    for word in array_of_words:
-        if word in dic:
-            return word
 
-        else:
-            dic[word] = 0
-
-
-string = 'Once upon a time, there was a brave princess who'
-first_repeated_word = repeated_word(string)
-print(first_repeated_word)
+print(repeated_word("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only"))
